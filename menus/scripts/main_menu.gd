af8ef9ea63a9a7badd5969be2392bfef081d2b62
Bryan Tait <bryan.b.tait@gmail.com>
2024-11-28 22:09:07 -0500

    voidhold init
    main menu structure with panning background

---
+++ menus/scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_play_pressed() -> void:
+    # get_tree().change_scene_to_file("res://game.tscn")
+    pass
+
+func _on_settings_pressed() -> void:
+    get_tree().change_scene_to_file("res://menus/scenes/settings_menu.tscn")
+    pass # Replace with function body.
+
+func _on_credits_pressed() -> void:
+    pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+    get_tree().quit()
